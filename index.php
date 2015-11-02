772089eb76b43f9d44e85b5bdecc92aa7733e8dd
Lloyd Sanchez <lloyd.v.sanchez@copypaste.ph>
2015-11-03 04:45:13 +0800

    adding initial files

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if IE 9 ]><html class="ie ie9" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]> <!-->
+<html class="skrollr skrollr-desktop" lang="en"><!--<![endif]-->
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+<meta charset="utf-8">
+
+<title>VALENS (PRP) Face, Hair and Musculoskeletal PRP</title>
+<!-- favicon -->
+<link rel="shorcut icon" href="images/favicon.ico" type="favicon">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/styles.css">
+
+<script type="text/javascript">
+	function scrollT(id, p){
+		p = typeof p !== 'undefined' ? p : 50;
+		console.log(p);
+		$(id).animatescroll( {padding:p} );
+
+		$("#bs-example-navbar-collapse-1").collapse('hide');
+	}
+</script>
+
+</head>
+
+<body class="">
+  <header class="large">
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <img src="images/logo.jpg" style="float:left;padding: 10px 5px 5px;">
+            <a class="navbar-brand" href="#" onclick="scrollT('#home', 70);">VALENS (PRP)</a>
+          </img>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+            <li><a href="#" onclick="scrollT('#home', 70);">Home</a></li>
+            <li><a href="#" onclick="scrollT('#about');">About</a></li>
+						<li><a href="#" onclick="scrollT('#procedures');">Procedures</a></li>
+            <li><a href="#" onclick="scrollT('#contact');">Contact</a></li>
+          </ul>
+				  <ul class="nav navbar-nav navbar-right fa">
+            <li><a href="#"><i class="fa fa-twitter"></i><span>Twitter</span></a></li>
+						<li><a href="#"><i class="fa fa-facebook-square"></i><span>Facebook</span></a></li>
+						<li><a href="#"><i class="fa fa-instagram"></i><span>Instagram</span></a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+	</header>
+
+	<!-- home section -->
+	<section id="home">
+		<div class="container">
+			<div class="header-group">
+				<h1><span class="fn">VALENS (PRP)</span></h1>
+				<h2><span class="fn">Face, Hair and Musculoskeletal PRP</span></h2>
+				<a class="button" href="#contact" onclick="scrollT('#contact');">Contact me</a>
+			</div>
+		</div>
+	</section>
+
+	<!-- about section -->
+	<section id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 text-center">
+					<img src="images/p12.jpg"/>
+				</div>
+				<div class="col-sm-7">
+					<p><strong>What is Platelet-Rich Plasma or PRP?</strong></p>
+					<p>Platelet-Rich Plasma or PRP is derived from the patient’s blood. Processing the patient’s blood specimen in a centrifuge separates the blood components and concentrates the platelets in the plasma. The concentrated platelets found in PRP are rich in growth factors along with bioactive proteins that are vital to initiate and accelerate tissue repair and regeneration. These bioactive proteins increase stem cell production to initiate connective tissue healing, bone regeneration and repair, promote development of new blood vessels, and stimulate the wound healing process.</p>
+					<p><strong>What are the potential benefits of treatment?</strong></p>
+					<p>Patients may see a significant improvement in symptoms, as well as a remarkable return of function. This may eliminate the need for more aggressive treatments such as long-term medication use or surgery.</p>
+					<p><strong>How many PRP therapy treatments are needed?</strong></p>
+					<p>While response to the therapy will vary, each patient’s are required 4 sessions. Each sessions is spaced approximately one week apart. There is no limit to the number of treatments a person can have. The risks and side effects do not change with the number of injections.</p>
+					<p><strong>How soon can I go back to regular activities?</strong></p>
+					<p>PRP therapy helps regenerate tendons and ligaments, but it is not a quick fix. This therapy is stimulating the growth of new tissue, requiring time and rehabilitation. During the treatment process, most people are able to resume normal activities and exercise.</p>
+					<p style="padding-top: 20px">We have registered doctors and nurses to facilitate your needs.</p>
+					<p>We are located in Cebu City, Philippines.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- procedures section -->
+	<section id="procedures">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2><span class="glyphicon glyphicon-blackboard" aria-hidden="true"></span>PROCEDURES</h2>
+					<ul class="nav nav-tabs">
+		        <li class="nav active"><a href="#A" data-toggle="tab"><span class="glyphicon glyphicon-tint" aria-hidden="true"></span>&nbsp;FAQ Platelet Rich Plasma</a></li>
+		        <li class="nav"><a href="#B" data-toggle="tab"><span class="glyphicon glyphicon-lamp" aria-hidden="true"></span>&nbsp;Musculoskeletal PRP Treatment</a></li>
+		        <li class="nav"><a href="#C" data-toggle="tab"><span class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></span>&nbsp;HAIR AND FACE PRP Treatment</a></li>
+					</ul>
+			    <!-- Tab panes -->
+			    <div class="tab-content">
+		        <div class="tab-pane fade in active" id="A">
+							<div class="col-sm-5 text-center">
+								<img src="images/musculo.jpg"/>
+							</div>
+							<div class="col-sm-7">
+								<p><strong>Is PRP safe and affordable?</strong></p>
+								<p>Yes, the risk is minimal because the treatment uses your own blood platelets and plasma, thereby eliminating the risk for disease transmission and your body rejecting it. Very affordable and it is alternative to surgery</p>
+								<p><strong>What Conditions are treated with PRP?</strong></p>
+								<p>PRP Injections can treat a number of conditions for the hip, knee, arm, shoulder, lower leg and feet. It is best to consult with your physician for a complete listing. However, below is an abbreviated list of conditions that can be treated with PRP.</p>
+								<ol>
+									<li>Acute muscle tears and strains</li>
+									<li>Chronic Tendinopathies (tennis elbow, Achilles tendon, patellar tendon and rotator cuff)</li>
+									<li>Ligament injuries (ie: medial or lateral collateral ligament—MCL/LCL)</li>
+									<li>Osteoarthritis</li>
+									<li>Psoriasis</li>
+									<li>Chronic Wound Diabetes</li>
+								</ol>
+								<p><strong>What are the potential benefits of treatment?</strong></p>
+								<p>Patients may see a significant improvement in symptoms, as well as a remarkable return of function. This may eliminate the need for more aggressive treatments such as long-term medication use or surgery.</p>
+							</div>
+						</div>
+		        <div class="tab-pane fade" id="B">
+							<div class="col-sm-6">
+								<p><strong>Musculoskeletal PRP Treatment</strong></p>
+								<p>PRP can be injected into:</p>
+								<ol>
+									<li>Tennis elbow (common extensor tendinosis)</li>
+									<li>Golfer’s elbow (medial epicondylitis)</li>
+									<li>Jumper’s knee (patellar tendinosis)</li>
+									<li>Achilles tendinosis</li>
+								</ol>
+								<p><strong>What other injuries can be treated with PRP?</strong></p>
+								<p>PRP can also be used in:</p>
+								<ol>
+									<li>Trochanteric bursitis</li>
+									<li>Knee MCL tears</li>
+									<li>Knee osteoarthritis</li>
+								</ol>
+							</div>
+							<div class="col-sm-6 text-center">
+								<img src="images/baldy.gif"/>
+							</div>
+						</div>
+		        <div class="tab-pane fade" id="C">
+							<div class="col-sm-4 text-center">
+								<img src="images/crappy_face.jpg"/>
+							</div>
+							<div class="col-sm-8">
+								<p><strong>Hair Loss Enhancement</strong></p>
+								<p>PRP (Platelet Rich Plasma) for Hair Loss can help you to prevent further hair by stimulating the growth of a thicker hair strand, slowing hair loss while promoting healthier and thicker hair growth.</p>
+								<p>PRP for Hair Loss acts like a fertilizer on your hair follicles, which are the seeds you need in order for the hair to grow, it can help to promote health and strength of a thinning hair.</p>
+								<p><strong>FACE PRP Treatment</strong></p>
+								<p>PRP (Platelet Rich Plasma) stimulates your own collagen to grow for total facial rejuvenation rather than individual wrinkle improvement. PRP is used for volumizing faces that are beginning to look drawn, to plump out cheek indentations, soften under eye hollows, improve the skin tone, tightness, and texture.</p>
+								<p>It is profoundly termed as VAMPIRE FACELIFT. Smoother skin, no surgery- no garlic or bite marks not required.</p>
+							</div>
+						</div>
+			    </div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="column col-sm-4 col-sm-offset-1">
+					<h2><span class="glyphicon glyphicon-blackboard" aria-hidden="true"></span>&nbsp;CONTACT US</h2>
+					<p>Got any question/s or just want to say 'hi'? Contact us.</p>
+					<hr>
+					<p>VALENS (PRP) - Face, Hair and Musculoskeletal PRP</p>
+						<ul>
+							<li><p>Room 304, M Diaz Building Avila Street, Cebu City, Philippines, 6000</p></li>
+							<li><p>+63 9330257099/+63 32 266 5011</p></li>
+							<li><p><a href="mailto:prp_centre@asia.com">prp_centre@asia.com</a></p></li>
+						</ul>
+				</div>
+
+				<div style="opacity: 1;" class="column col-sm-6">
+
+					<form id="form"method="post" action="">
+						<input tabindex="1" name="Name" id="name" placeholder="Name (*)" type="text">
+						<input tabindex="2" name="email" id="email" placeholder="Email (*)" type="text">
+						<textarea name="message" cols="10" rows="5" id="message" placeholder="Your message here..."></textarea>
+						<input type="submit" class="button" value="send" onclick="alert('wala pa');return false;" style="padding:10px 20px 10px; background-color:#958E87"/>
+					</form>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+		<p>Copyright © 2015 <span><strong>&nbsp; - &nbsp;</strong>VALENS (PRP)</span></p>
+	</footer>
+
+	<!-- script -->
+  <script src="js/jquery-1.11.3.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+	<script src="js/animatescroll.min.js"></script>
+	<script>
+		$(document).ready(function() {
+
+		});
+	</script>
+
+  <!--script src="js/npm.js"></script-->
+</body></html>
